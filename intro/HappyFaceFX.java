05ccf95fd7ad9673fd645f8453b1aa08aeaf993e
Frederik Gailly <Frederik.Gailly@ugent.be>
2022-09-20 21:25:12 +0200

    add intro

---
+++ intro/HappyFaceFX.java
@@ -0,0 +1,43 @@
+
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.shape.ArcType;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author fgailly
+ */
+public class HappyFaceFX extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root =new Group();
+        Scene scene = new Scene(root);
+        Canvas canvas = new Canvas(400, 300);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.strokeOval(100, 50, 200, 200);
+        gc.fillOval(155, 100, 10, 20);
+        gc.fillOval(230, 100, 10, 20);
+        gc.strokeArc(150, 160, 100, 50, 180, 180, ArcType.OPEN);
+
+        root.getChildren().add(canvas);
+        primaryStage.setTitle("HappyFace in JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
